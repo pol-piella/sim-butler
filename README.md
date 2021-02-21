860f15df9ce357082423f7b18f16225ebc2dfcfe
Pol Piella <info@polpiellamusic.com>
2021-02-21 18:19:58 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sim-butler
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import sim_butlerTests
+
+var tests = [XCTestCaseEntry]()
+tests += sim_butlerTests.allTests()
+XCTMain(tests)
